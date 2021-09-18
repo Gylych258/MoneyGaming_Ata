91680e634abccc618e5adcea03805aaf97714b6d
Atagylych <agylych@gmail.com>
2021-09-19 00:37:07 +0800

    Ata Branch commit

---
+++ src/test/java/com/MoneyGaming/tests/AtaTest.java
@@ -0,0 +1,14 @@
+package com.MoneyGaming.tests;
+
+import org.testng.annotations.Test;
+
+public class AtaTest {
+
+    @Test
+
+    public void testAta(){
+
+        System.out.println("My beautiful test");
+    }
+
+}
